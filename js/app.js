b6ea242c4a8b3bfb9ef5202c1b888e090e12bdf4
darkxxx2000 <silvana1002010@gmail.com>
2026-04-14 16:02:44 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
+let selectedCategory = "generic";
+
+document.querySelectorAll(".categories button").forEach(btn=>{
+  btn.addEventListener("click", () => {
+    selectedCategory = btn.dataset.cat;
+  });
+});
 document.getElementById("generateButton").addEventListener("click", () => {
   const text = document.getElementById("description").value;
 
   if (!text) return;
 
-  const { ctx, results } = generate(text);
+  const { ctx, results } = generate(text + " " + selectedCategory);
 
   render(results, ctx);
 });
