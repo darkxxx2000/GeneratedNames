11c32473c8a794021a10b6237614e8fb1929d741
darkxxx2000 <silvana1002010@gmail.com>
2026-04-15 11:03:43 -0300

    Update renderer.js

--- js/renderer.js
+++ js/renderer.js
@@ -1,21 +1,22 @@
-function render(results, ctx){
+function render(data, ctx){
   const container = document.getElementById("results");
   container.innerHTML = "";
 
-  results.forEach(name => {
+  data.forEach(item => {
+
     const card = document.createElement("div");
     card.className = "card";
 
     card.innerHTML = `
       <div>
-        <div class="name">${name}</div>
-        <div class="tag">${ctx}</div>
+        <div class="name">${item.name}</div>
+        <div class="tag">${ctx} • score ${item.score}</div>
       </div>
       <button class="copy">Copy</button>
     `;
 
     card.querySelector(".copy").onclick = () => {
-      navigator.clipboard.writeText(name);
+      navigator.clipboard.writeText(item.name);
     };
 
     container.appendChild(card);
